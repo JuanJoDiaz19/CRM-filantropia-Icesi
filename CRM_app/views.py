956290d6bca01d5586657e00d36afbbc1c79e4dd
Juan Jose Diaz <juajodp1903@gmail.com>
2023-09-15 20:50:26 -0500

    feat: adding the basic strucutre of register users

--- CRM_app/views.py
+++ CRM_app/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login 
+from django.db import IntegrityError
+
 
 # Create your views here.
+ 
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+         return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
+
+    else : 
+        if(request.POST['password1'] == request.POST['password2']):
+            # Register User
+            try: 
+                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm, 
+                    'error' : 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm, 
+            'error' : 'Password do not match'
+        })
+    
+def tasks(request):
+    return render(request, 'tasks.html')
+
+
+   
--- CRM_filantropia_ICESI/urls.py
+++ CRM_filantropia_ICESI/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CRM_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('signup/', views.signup, name="signup"),
+    path('tasks/', views.tasks, name="tasks")
 ]
