eff99fbc2e6867be92f5822e631f01ef43b3269d
Juan Jose Diaz <juajodp1903@gmail.com>
2023-09-28 16:17:50 -0500

    feat: adding the html of the donations page

--- CRM_filantropia_ICESI/urls.py
+++ CRM_filantropia_ICESI/urls.py
@@ -32,6 +32,7 @@ from CRM_app.views.investigations import Investigations
 from CRM_app.views.config import Config
 from CRM_app.views.ally import Ally
 from CRM_app.views.add_allie import Add_allie
+from CRM_app.views.donation import Donation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -51,4 +52,5 @@ urlpatterns = [
     path('delete_new/<int:new_id>/', DeleteNew.as_view(), name='delete_new'),
     path('allies/', Allies.as_view(), name='allies'),
     path('allies/<int:allie_id>/', Ally.as_view(), name="ally"),
+    path('donations/', Donation.as_view(), name="donation"),
 ]
