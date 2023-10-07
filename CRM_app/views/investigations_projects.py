00c790585693250e5094a943e2dc65aad9533751
Juan Jose Diaz <juajodp1903@gmail.com>
2023-10-07 15:34:20 -0500

    feat: making the view of the investigation projects

--- CRM_app/views/investigations_projects.py
+++ CRM_app/views/investigations_projects.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login,logout, authenticate
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from CRM_app.models import Investigation_Project
 
 class InvestigationsProjects(View):
     def get(self, request):
-        return render(request, 'investigation_projects.html')
+        investigation_projects = Investigation_Project.objects.all()  # Obtener todas las instancias de investiagation_projecs
+        return render(request, 'investigation_projects.html', {'investigation_projects': investigation_projects}) 
