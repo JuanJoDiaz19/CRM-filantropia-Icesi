ad52dd032ca8aa555720096b0e87816773b4f526
Santiago0212004 <santiago.barraza1@u.icesi.edu.co>
2023-10-16 13:23:53 -0500

    feat: Calendar of events and meeting finished with filter by ally

--- CRM_app/views/calendar.py
+++ CRM_app/views/calendar.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views import View
-from CRM_app.models import Event, Allie
+from CRM_app.models import Event, Allie, Meeting
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
 class Calendar(View):
-    def get(self, request):
-        events = Event.objects.all()
+    def get(self, request, allie_id):
+
         events_data = []
+
+        if allie_id != "all":
+            events = Event.objects.filter(eventallie__allie_id=int(allie_id))
+            meetings = Meeting.objects.filter(allie_id=int(allie_id))
+        else:
+            events = Event.objects.all()
+            meetings = Meeting.objects.all()
 
         for event in events:
             allies = list(Allie.objects.filter(eventallie__event=event).values('name'))
@@ -18,7 +25,19 @@ class Calendar(View):
                 'description': event.description,
                 'objective': event.objective,
                 'start': event.date.strftime('%Y-%m-%d'),  # Convert date to string
-                'allies': allies_names
+                'allies': allies_names,
+                'color': 'blue'  # Color para eventos
+            })
+
+        for meeting in meetings:
+            events_data.append({
+                'title': meeting.title,
+                'event_type': meeting.meeting_type_id.name,
+                'description': meeting.description,
+                'objective': meeting.objective,
+                'start': meeting.date.strftime('%Y-%m-%d'),  # Convert date to string
+                'allies': meeting.allie_id.name,
+                'color': 'red'  # Color para reuniones
             })
 
         return render(request, 'calendar.html', {'events_data': json.dumps(events_data, cls=DjangoJSONEncoder)})
