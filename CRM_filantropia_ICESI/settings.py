5c8bb70fd799603016d8ed45424e14aaeb5277ce
Santiago0212004 <santiago.barraza1@u.icesi.edu.co>
2023-11-24 14:09:43 -0500

    hotfix: deployment file

--- CRM_filantropia_ICESI/settings.py
+++ CRM_filantropia_ICESI/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
