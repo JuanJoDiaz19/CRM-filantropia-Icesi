b30ce1f2f92de06c986b53f92724cd8312a04c2d
Santiago0212004 <santiago.barraza1@u.icesi.edu.co>
2023-11-24 12:35:41 -0500

    deployment configuration

--- CRM_filantropia_ICESI/urls.py
+++ CRM_filantropia_ICESI/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from django.contrib import admin
 from django.urls import path
 from CRM_app.views.home import Home
@@ -100,3 +104,5 @@ urlpatterns = [
     path("cedep/", Cedep_marketing_home.as_view(), name="CEDEP_view"),
 ]
     
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
